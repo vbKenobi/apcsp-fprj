e37ce138cc3903355c3270252073196e7775391a
ybKenobi <58750962+ybKenobi@users.noreply.github.com>
2020-01-16 12:18:15 -0800

    Update ggoe.php

--- ggoe.php
+++ ggoe.php
@@ -35,12 +35,53 @@ margin: 4px 2px;
 curssor: pointer;
 width: 25%;
 
+body {
+  font-size: 28px;
+}
+
+ul {
+  list-style-type: none;
+  margin: 0;
+  padding: 0;
+  overflow: hidden;
+  background-color: #333;
+  position: -webkit-sticky; /* Safari */
+  position: sticky;
+  top: 0;
+}
+
+li {
+  float: left;
+}
+
+li a {
+  display: block;
+  color: white;
+  text-align: center;
+  padding: 14px 16px;
+  text-decoration: none;
+}
+
+li a:hover {
+  background-color: #111;
+  color: white;
+}
+
+.active {
+  background-color: lightblue;
+  color: black;
+}
 
 </style>
 
 
 
   <body>
+
+    <ul>
+  <li><a class="active" href="#home">Home</a></li>
+</ul>
+    
 
     <h1>Welcome</h1>
     <p>This program was made by Yajn B. and Bryton L. To access the derivative calcualtor please put dy in the upper bound and dx in the lower bound. You must enter all inputs for the program to work. </p>
