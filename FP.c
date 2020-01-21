8c4cad61921b34b42620c87bd5306155813d6698
leeb5737 <58750877+leeb5737@users.noreply.github.com>
2020-01-21 12:51:01 -0800

    Added more to algorithm

--- FP.c
+++ FP.c
@@ -3,7 +3,6 @@
 #include<stdlib.h>
 #include <string.h>
 #include <math.h>
-
 float Series(int co, int exp, int start, int end)
 {
 float f;
@@ -15,39 +14,40 @@ f = f * co;
 return f;
 }
 
+float point(int x, int co, int exp)
+{
+float f;
+f = pow(x, exp);
+f = f * co;
+return f;
+}
+
+
+
 int main(int argc, char *argv[])
 {
-
-float c1;
+float total = 0;
+float c1;   
 float e1;
-float c2;
+float c2; 
 float e2;
 char str[100];
 int len = 0;
 char *pstr = str;
+int ca = atoi(argv[1]);
+int cb = atoi(argv[2]);
 float endvalue;
-float total = 0;
-int eee, co, exp;
-if (argc < 11)
-{
-  printf("Not enough values (place 0 as value holders if you want nothing there).\n");
-}
-else
-{
+int eee;
 if (strcmp(argv[1], "dy")== 0 && strcmp(argv[2], "dx") == 0 && isdigit(*argv[3]) && isdigit(*argv[4]) && isdigit(*argv[5]) && isdigit(*argv[6]) && isdigit(*argv[7]) && isdigit(*argv[8]) && isdigit(*argv[9]) && isdigit(*argv[10]))
 {
-  printf("Derivtative Time \n");
-  
-  for ( int i = 3; i <= argc-2; i = i +2)
+    for ( int i = 3; i <= argc-2; i = i +2)
   {
-  
     c1 = atof(argv[i]);
     e1 = atof(argv[i+1]);
     if (c1 != 0 && e1 != 0)
     {
       c1 = c1*e1;
       e1 = e1 -1;
-    
       len = sprintf(pstr, " %f(X^%f) +", c1, e1);
       
       pstr += len;
@@ -60,28 +60,22 @@ if (strcmp(argv[1], "dy")== 0 && strcmp(argv[2], "dx") == 0 && isdigit(*argv[3])
   }
   str[strlen(str)-1] = '\0';
   printf("\n Derivative = %s\n", str);
+
   eee = 1;
 }
-  if (isdigit(*argv[1]) && isdigit(*argv[2]) && isdigit(*argv[3]) && isdigit(*argv[4]) && isdigit(*argv[5]) && isdigit(*argv[6]) && isdigit(*argv[7]) && isdigit(*argv[8]) && isdigit(*argv[9]) && isdigit(*argv[10]))
-  {
-    for (int i = 0; i < 4; i++)
-    {
-      total = Series(atoi(argv[3 + 2 * i]), atoi(argv[4 + 2 * i]), atoi(argv[1]), atoi(argv[2])) + total;
-    }
-    printf ("Summation value of %dx^%d+%dx^%d+%dx^%d+%dx^%d is: %f\n", atoi(argv[3]), atoi(argv[4]), atoi(argv[5]), atoi(argv[6]), atoi(argv[7]), atoi(argv[8]), atoi(argv[9]), atoi(argv[10]), total);
-  }
-  else if (eee == 1)
-  {
-    printf("");
-  }
-  else
-  {
-    printf("Invalid answers, try again.\n");
-  }
-  if (strcmp(argv[1], "anti-y")== 0 && strcmp(argv[2], "anti-x") == 0 && isdigit(*argv[3]) && isdigit(*argv[4]) && isdigit(*argv[5]) && isdigit(*argv[6]) && isdigit(*argv[7]) && isdigit(*argv[8]) && isdigit(*argv[9]) && isdigit(*argv[10]))
+else if (strcmp(argv[1], "x")== 0 && isdigit(*argv[2]) && isdigit(*argv[3]) && isdigit(*argv[4]) && isdigit(*argv[5]) && isdigit(*argv[6]) && isdigit(*argv[7]) && isdigit(*argv[8]) && isdigit(*argv[9]) && isdigit(*argv[10]))
 {
- 
-  for ( int i = 3; i <= argc-2; i = i +2)
+  for (int i = 0; i < 4; i++)
+  {
+  total = point(atoi(argv[2]), atoi(argv[3 + 2 * i]), atoi(argv[4 + 2 * i])) + total;
+  }
+  eee = 1;
+  printf ("Value of %dx^%d+%dx^%d+%dx^%d+%dx^%d at x = %d is: %4.2f\n", atoi(argv[3]), atoi(argv[4]), atoi(argv[5]), atoi(argv[6]), atoi(argv[7]), atoi(argv[8]), atoi(argv[9]), atoi(argv[10]), atoi(argv[2]), total);
+}
+else if (strcmp(argv[1], "anti-y")== 0 && strcmp(argv[2], "anti-x") == 0 && isdigit(*argv[3]) && isdigit(*argv[4]) && isdigit(*argv[5]) && isdigit(*argv[6]) && isdigit(*argv[7]) && isdigit(*argv[8]) && isdigit(*argv[9]) && isdigit(*argv[10]))
+{
+  eee = 1;
+for ( int i = 3; i <= argc-2; i = i +2)
   {
     c2 = atof(argv[i]);
     e2 = atof(argv[i+1]);
@@ -100,7 +94,22 @@ if (strcmp(argv[1], "dy")== 0 && strcmp(argv[2], "dx") == 0 && isdigit(*argv[3])
   }
   str[strlen(str)-1] = '\0';
   printf("\n Anti-Derivative = %s\n", str);
-
 }
-}
+  if (isdigit(*argv[1]) && isdigit(*argv[2]) && isdigit(*argv[3]) && isdigit(*argv[4]) && isdigit(*argv[5]) && isdigit(*argv[6]) && isdigit(*argv[7]) && isdigit(*argv[8]) && isdigit(*argv[9]) && isdigit(*argv[10]))
+  {
+    printf("Here is the Summation of the given values\n");
+    for (int i = 0; i < 4; i++)
+    {
+      total = Series(atoi(argv[3 + 2 * i]), atoi(argv[4 + 2 * i]), atoi(argv[1]), atoi(argv[2])) + total;
+    }
+    printf ("Summation value of %dx^%d+%dx^%d+%dx^%d+%dx^%d is: %4.2f\n", atoi(argv[3]), atoi(argv[4]), atoi(argv[5]), atoi(argv[6]), atoi(argv[7]), atoi(argv[8]), atoi(argv[9]), atoi(argv[10]), total);
+  }
+  else if (eee == 1)
+  {
+    printf("");
+  }
+  else
+  {
+    printf("Invalid answers, try again.\n");
+  }
 }
